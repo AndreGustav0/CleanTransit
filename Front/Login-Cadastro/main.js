cc151f4fc6943980d69f4ecc1a3a875d810a61a1
André Gustavo <andregustavodeoliveira49@gmail.com>
2024-06-06 01:47:18 -0300

    tentativa 1 validação
    aqui tentei atribuir alguns alertar para se caso o usuário tentasse acessar ao programa sem digitar nenhum dos campos solicitados.

--- Front/Login-Cadastro/main.js
+++ Front/Login-Cadastro/main.js
@@ -10,4 +10,57 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-/* --------------------------------- Home Page------------------------------- */
+/* --------------------------------- Validação ------------------------------- */
+
+/*  Validação Cadastrar  
+const formCadastrar = document.getElementById("formCadastrar");
+const razaoSocialCadastroInput = document.getElementById("razaoSocialCadastrar");
+const cnpjInput = document.getElementById("cnpj");
+const emailCadastroInput = document.getElementById("emailCadastro");
+const senhaCadastroInput = document.getElementById("senhaCadastro");
+
+/*  Validação entrar  
+const formEntrar = document.getElementById("formEntrar");
+const razaoSocialEntrarInput = document.getElementById("razaoSocialEntrar");
+const emailEntrarInput = document.getElementById("emailEntrar");
+const senhaEntrarInput = document.getElementById("senhaEntrar");
+
+formCadastrar.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    if(razaoSocialCadastroInput.value === ""){
+        alert("Por favor, preencha o campo Razão Social.");
+        return;
+    }
+    if(cnpjInput.value === ""){
+        alert("Por favor, preencha o campo CNPJ.");
+        return;
+    }
+    if(emailCadastroInput.value === ""){
+        alert("Por favor, preencha o campo Email.");
+        return;
+    }
+    if(senhaCadastroInput.value === ""){
+        alert("Por favor, preencha o campo Senha para gerar o seu cadastro.");
+        return;
+    }
+});
+
+formEntrar.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    
+    if(razaoSocialEntrarInput.value === ""){
+        alert("Por favor, preencha o campo Razão Social.");
+        return;
+    }
+    if(emailEntrarInput.value === ""){
+        alert("Por favor, preencha o campo Email.");
+        return;
+    }
+    if(senhaEntrarInput.value === ""){
+        alert("Por favor, preencha o campo Senha.");
+        return;
+    }
+});
+
+*/
